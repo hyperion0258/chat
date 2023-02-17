efdd9debe1ca0c73734a944f68fc15a5dbdc82d0
희윤 김 <hyperion0258@gmail.com>
2023-02-17 18:39:27 +0900

    [김희윤] - master : first commit

---
+++ src/main/java/com/softbridge/chat/controller/RoomController.java
@@ -0,0 +1,38 @@
+package com.softbridge.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RoomController {
+
+    @GetMapping("/sendbird/room01")
+    public String sendbird01() {
+        return "roomtype01";
+    }
+
+    @GetMapping("/sendbird/room02")
+    public String sendbird02() {
+        return "roomtype02";
+    }
+
+    @GetMapping("/sendbird/room03")
+    public String sendbird03() {
+        return "roomtype03";
+    }
+
+    @GetMapping("/talkplus/room01")
+    public String talkplus01() {
+        return "roomtype01";
+    }
+
+    @GetMapping("/talkplus/room02")
+    public String talkplus02() {
+        return "roomtype02";
+    }
+
+    @GetMapping("/talkplus/room03")
+    public String talkplus03() {
+        return "roomtype02";
+    }
+}
