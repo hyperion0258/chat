f28016f7639e99aeca3ee0f376533396e33d64a7
희윤 김 <hyperion0258@gmail.com>
2023-02-24 10:23:14 +0900

    [김희윤] - master : talkplus 주석 작성

--- src/main/resources/static/js/chat_talkplus.js
+++ src/main/resources/static/js/chat_talkplus.js
@@ -1,3 +1,4 @@
+// appId는 talkplus에서 제공 하는 고유 아이디로서 30일(2023-03-14까지) 사용 가능, 그 이후에 사용하려면 유료결제가 필요함
 const appId = "2052a7e3-6110-4a2a-8cfd-ae58d3f57394"
 let userId = ""
 let userName = ""
@@ -5,17 +6,19 @@ let password = ""
 let loginToken = ""
 const channelName = ""
 const channelId = ""
+// talkplus 초기화 appId 필요
 const client = new TalkPlus.Client({appId: appId});
+// pathName은 url의 경로와 그앞의 / 로 이루어진 문자열을 반환하는데 / 는 공백으로 변경
 let pathName = (window.location.pathname).replaceAll("/", "").toString();
 
 $(document).ready(function() {
-    // 이벤트 핸들러
+    // 이벤트 감지 핸들러
     client.on('event', function (data) {
         // 이벤트 타입이 메세지 일경우 상대방 메세지 받기, 스크롤다운
         if (data.type === 'message') {
             // 상대방 메세지 받기
             receiveMessage(data.message)
-            // 스크롤 다운
+            // 상대방 메세지를 받을때 스크롤 다운
             scrollDown()
         }
     })
@@ -23,8 +26,7 @@ $(document).ready(function() {
     start();
     // 사용자가 글작성해서 엔터치면 서버에 전달
     writerSendMessage();
-    // 방 입장시 스크롤다운되게끔
-    scrollDown();
+    // 닉네임 변경 길이 체크 함수
     lengthCheck();
 });
 
@@ -151,20 +153,25 @@ function start() {
         })
     }
 }
-
+// 사용자가 엔터를치면 input태그를 비워주고 서버에 메세지를 보내는 함수를 호출
 function writerSendMessage() {
-    $(document).on('keypress', '.enterMessage', function (e) {
+    $(document).keypress(function (e) {
         if ($('.enterMessage').val() !== ''){
+            // keyCode 13 = enter
             if (e.keyCode === 13) {
+                // preventDefault는 input태그의 고유동작(페이지 리로드)을 중지시킴
                 e.preventDefault();
                 const message = $('.enterMessage').val();
+                // 엔터키 입력후 input태그의 내용을 비워줌
                 $('.enterMessage').val('');
+                // 서버에 메세지를 보내는 함수를 호출
                 writerSendMessageToServer(message);
             }
         }
     });
 }
 
+// 사용자가 쓴 메세지를 서버에 보내는 함수
 function writerSendMessageToServer(message) {
     client.sendMessage({
         channelId: pathName,
@@ -177,12 +184,16 @@ function writerSendMessageToServer(message) {
             return alert(JSON.stringify(errResp));
         }
         let html = '';
+        // 서버에 메세지를 보내고 응답으로 받은 메세지를 html에 넣는다
         html = writerSendMessageAddHtml(data.message);
+        // chatArea에 append
         $('.chatArea').append(html);
+        // 사용자가 메세지를 입력했을때 스크롤 다운
         scrollDown();
     });
 }
 
+// 사용자가 쓴 메세지를 html에 넣는 함수
 function writerSendMessageAddHtml(message) {
     let template = '';
     template = `<div class="writer">
@@ -192,6 +203,7 @@ function writerSendMessageAddHtml(message) {
     return template;
 }
 
+// 다른 사용자가 쓴 메세지를 html에 넣는 함수
 function othersSendMessageAddHtml(message) {
     let template = '';
     template = `<div class="others">
@@ -201,36 +213,44 @@ function othersSendMessageAddHtml(message) {
     return template;
 }
 
+// 채널에 작성된 메세지들을 html에 붙이는 함수 파라미터 messages는 배열
 function getAllMessages(messages) {
+    // messages가 배열이므로 for문으로 하나씩 꺼낸다
     for (let i = messages.length - 1; i >= 0; i--) {
         const message = messages[i]
         let html = '';
+        // getAllMessages 함수는 client.getMessages 에서 호출되는데 client.getMessages가 성공하면 messages에 userId가 포함된 정보가 넘어온다, 그리고 로컬스토리지의 userId와 비교한다
         if(message.userId === localStorage.getItem('userId').replaceAll("\"", "")) {
+            // 로컬스토리지의 userId와 client.getMessages 에서 넘어오는 userId가 일치하면 내가쓴것이므로 사용자가 쓴 메세지를 불러온다
             html = writerSendMessageAddHtml(message);
             $('.chatArea').append(html);
         } else {
+            // 로컬스토리지의 userId와 client.getMessages 에서 넘어오는 userId가 일치하지 않으면 내가쓴것이 아니므로 다른 사용자가 쓴 메세지를 불러온다
             html = othersSendMessageAddHtml(message);
             $('.chatArea').append(html);
         }
-
     }
+    // 채널에 작성된 메세지들을 불러오고 스크롤 다운
     scrollDown();
 }
 
+// 이벤트 감지 핸들러 내부에 있는 함수로 상대방의 메시지를 받는 함수
 function receiveMessage(message) {
     $('.chatArea').append(othersSendMessageAddHtml(message));
 }
 
-
+// 로그아웃 함수(테스트용)
 function logOut() {
     client.logout();
 }
 
+// 로그인 상태를 확인하는 함수, 로그인됬으면 true 로그인되지않았으면 false(테스트용)
 function isLoggedIn() {
     let isLoggedIn = client.isLoggedIn();
     console.log(isLoggedIn);  // true or false
 }
 
+// 채널에서 나가는 함수(테스트용)
 function leaveChannel() {
     client.leaveChannel({
         channelId: pathName
@@ -243,21 +263,27 @@ function leaveChannel() {
         })
 }
 
+// 랜덤 아이디를 생성하는 함수
 function generateRandomId() {
     return generateRandomString() + '_' + generateRandomString() + '_' + generateRandomString();
 }
 
-
+// 랜덤 아이디를 생성하기 위해 랜덤 스트링을 생성하는 함수
 function generateRandomString() {
+    // Math.floor() = 숫자 내림, Math.random() = 0~1 사이의 랜덤한 수, 0x10000 = 십육진법 수, 10진법으로는 65536
     return Math.floor((1 + Math.random()) * 0x10000)
+        // toString(16)은 10진수를 16진법으로 바꿔준다
         .toString(16)
+        // substring(1)은 시작 인덱스(1)부터 문자열 끝부분 까지 문자열을 반환
         .substring(1);
 }
 
+// 스크롤 다운 함수, 채팅을 치거나, 채널에 참가했을때 호출
 function scrollDown() {
     $('.chatArea').scrollTop($('.chatArea')[0].scrollHeight);
 }
 
+// 사용자 닉네임 변경 함수
 function updateUser() {
     let newUserName = $('.changeNameInput').val();
     client.updateUser({
@@ -266,11 +292,13 @@ function updateUser() {
     alert("닉네임이 " + newUserName + " (으)로 변경되었습니다")
 }
 
+// 사용자 닉네임 변경시 길이 체크(최대 8글자)해서 경고문 띄우는 함수
 function lengthCheck() {
     $(".changeName > input").keydown(function (e) {
-            let inputLength = $(this).val().length;
-            if(inputLength >= 8) {
-                alert("닉네임은 8글자를 초과할 수 없습니다")
-            }
+        // this는 $(".changeName > input")
+        let inputLength = $(this).val().length;
+        if(inputLength > 8) {
+            alert("닉네임은 8글자를 초과할 수 없습니다")
+        }
     })
 }
